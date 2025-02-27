1f4cf14b21ef30b1462b9a5d912e5b9f96f44f57
Alvaro David Gonzalez Ballesteros <AG9181NI@grupomonge.com>
2025-02-27 08:56:15 -0600

    Capítulo 7: Haciendo el fetching de datos

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -66,7 +66,7 @@ export async function fetchCardData() {
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
-    ]);
+    ]); //Parallel data fetching
 
     const numberOfInvoices = Number(data[0][0].count ?? '0');
     const numberOfCustomers = Number(data[1][0].count ?? '0');
