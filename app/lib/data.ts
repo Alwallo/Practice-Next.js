3dbc27118d2560a81743ad7686b55a80e1371827
alwallo <alvgonzalezb@gmail.com>
2025-02-27 20:35:13 -0600

    Capítulo 9: Streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,18 +16,22 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
+    if (error instanceof AggregateError) {
+      error.errors.forEach(err => console.error('Inner error:', err));
+    }
+    throw new Error(String(error));
   }
+  
 }
 
 export async function fetchLatestInvoices() {
